80befc01a736ba8deeb69d3b42930d597fb92c4a
Laura Tannahill <lntannahill@gmail.com>
2021-01-04 20:33:14 +0000

    Add bonus validation ex 7

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,12 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: -1 }
+  validate :must_carry_men_or_womens
+
+  def must_carry_men_or_womens
+    if !mens_apparel && !womens_apparel
+      errors.add(:womens_apparel, ": Store must carry at least one of men's or women's styles")
+      errors.add(:mens_apparel, ": Store must carry at least one of men's or women's styles")
+    end
+  end
 end
