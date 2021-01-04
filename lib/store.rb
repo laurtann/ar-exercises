44afebd67e989a7f95bc8c2d736d2d22db460f87
Laura Tannahill <lntannahill@gmail.com>
2021-01-04 22:27:42 +0000

    Complete ex 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,10 +4,19 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true, greater_than: -1 }
   validate :must_carry_men_or_womens
 
+  before_destroy :selective_destroy
+
   def must_carry_men_or_womens
     if !mens_apparel && !womens_apparel
       errors.add(:womens_apparel, ": Store must carry at least one of men's or women's styles")
       errors.add(:mens_apparel, ": Store must carry at least one of men's or women's styles")
     end
   end
+
+  private
+    def selective_destroy
+      if self.employees.size > 0
+        return false
+      end
+    end
 end
